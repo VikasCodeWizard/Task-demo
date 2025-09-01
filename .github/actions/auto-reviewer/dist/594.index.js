5e73a6d33b4e8f6c2e4ab0ae2a47635ba740af84
Vikas-shuk <vikas.shukla.ece14@itbhu.ac.in>
2025-09-01 19:31:02 +0530

    Revert "build(auto-reviewer): sync dist with updated prompt from planto-reviewer"
    This reverts commit b3d4e82706fec6b75149558314f4721763707e08.

--- .github/actions/auto-reviewer/dist/594.index.js
+++ .github/actions/auto-reviewer/dist/594.index.js
@@ -3,6 +3,40 @@ exports.id = 594;
 exports.ids = [594];
 exports.modules = {
 
+/***/ 59107:
+/***/ ((__unused_webpack_module, exports) => {
+
+var __webpack_unused_export__;
+
+
+__webpack_unused_export__ = ({
+  value: true
+});
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+var isBrowser = typeof window !== "undefined" && typeof window.document !== "undefined";
+
+var isNode = typeof process !== "undefined" && process.versions != null && process.versions.node != null;
+
+var isWebWorker = (typeof self === "undefined" ? "undefined" : _typeof(self)) === "object" && self.constructor && self.constructor.name === "DedicatedWorkerGlobalScope";
+
+/**
+ * @see https://github.com/jsdom/jsdom/releases/tag/12.0.0
+ * @see https://github.com/jsdom/jsdom/issues/1537
+ */
+var isJsDom = typeof window !== "undefined" && window.name === "nodejs" || typeof navigator !== "undefined" && (navigator.userAgent.includes("Node.js") || navigator.userAgent.includes("jsdom"));
+
+var isDeno = typeof Deno !== "undefined" && typeof Deno.version !== "undefined" && typeof Deno.version.deno !== "undefined";
+
+__webpack_unused_export__ = isBrowser;
+__webpack_unused_export__ = isWebWorker;
+exports.UG = isNode;
+__webpack_unused_export__ = isJsDom;
+__webpack_unused_export__ = isDeno;
+
+/***/ }),
+
 /***/ 594:
 /***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
 
@@ -1092,7 +1126,7 @@ class ChatOpenAI extends BaseChatModel {
             this.client = new dist.OpenAIApi(clientConfig);
         }
         const axiosOptions = {
-            adapter: lib.isNode ? undefined : fetchAdapter,
+            adapter: lib/* isNode */.UG ? undefined : fetchAdapter,
             ...this.clientConfig.baseOptions,
             ...options,
         };
